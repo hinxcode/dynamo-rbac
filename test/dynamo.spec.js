8c686533b7e3361673eae7d901b9cf6513b23168
hinxcode <b2765270@gmail.com>
2017-03-08 13:40:43 +0800

    Spec

--- test/dynamo.spec.js
+++ test/dynamo.spec.js
@@ -1,40 +1,71 @@
 import should from 'should'
-import AWS from 'aws-sdk'
+import { connectDB } from '../src/dynamo'
 
-AWS.config.update({
-  region: 'us-west-2',
-  endpoint: 'http://localhost:8000'
-})
+describe('table User', function () {
+  let dynamodb
+  const tableName = 'User'
 
-const dynamodb = new AWS.DynamoDB()
-
-describe.skip('dynamoDB table', function () {
-  const params = {
-    TableName: 'Movies',
-    KeySchema: [
-      { AttributeName: 'year', KeyType: 'HASH' },  // Partition key
-      { AttributeName: 'title', KeyType: 'RANGE' }  // Sort key
-    ],
-    AttributeDefinitions: [
-      { AttributeName: 'year', AttributeType: 'N' },
-      { AttributeName: 'title', AttributeType: 'S' }
-    ],
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 10,
-      WriteCapacityUnits: 10
-    }
-  }
+  it('should connect to db without error', function () {
+    dynamodb = connectDB('us-west-2', 'http://localhost:8000')
+    should.exist(dynamodb)
+  })
 
   it('should not be found before we create it', function (done) {
-    dynamodb.describeTable({TableName: params.TableName}, function (err, data) {
-      should.exist(err)
-      err.code.should.equal('ResourceNotFoundException')
-      done()
+    dynamodb.describeTable({TableName: tableName}, function (err, data) {
+      if (err) {
+        should.exist(err)
+        err.code.should.equal('ResourceNotFoundException')
+        done()
+      } else {
+        dynamodb.deleteTable({TableName: tableName}, function (err, data) {
+          should.not.exist(err)
+          done()
+        })
+      }
     })
   })
 
   it('should be created without error', function (done) {
+    const params = {
+      TableName: tableName,
+      KeySchema: [
+        { AttributeName: 'id', KeyType: 'HASH' },
+        { AttributeName: 'Role', KeyType: 'RANGE' }
+      ],
+      AttributeDefinitions: [
+        { AttributeName: 'id', AttributeType: 'S' },
+        { AttributeName: 'Role', AttributeType: 'N' }
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+      }
+    }
+
     dynamodb.createTable(params, function (err, data) {
+      should.not.exist(err)
+      done()
+    })
+  })
+
+  it('should put a new item without error', function (done) {
+    const params = {
+      TableName: tableName,
+      Item: {
+        'id': {
+          S: 'id001'
+        },
+        'Role': {
+          N: '1'
+        },
+        'AccountName': {
+          S: 'Ben Y Huang'
+        }
+      },
+      ReturnConsumedCapacity: 'TOTAL'
+    }
+
+    dynamodb.putItem(params, function (err, data) {
       should.not.exist(err)
       done()
     })
